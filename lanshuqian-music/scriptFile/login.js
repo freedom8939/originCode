3d723d4a8afc7cd6b8f2d79bd553b62bec517616
lanshuqian <89398368@qq.com>
2024-03-08 16:32:49 +0800

    完成导航条，添加jquery。

---
+++ lanshuqian-music/scriptFile/login.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#loginButton').click(function(){
+        alert('登录按钮被点击了！');
+    });
+
+
+    $('#searchItem').click(function(){
+        alert('搜索按钮被点击了！');
+    });
+});
