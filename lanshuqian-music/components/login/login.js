059e6303929219582418a25d62fac09f3ac51da6
lanshuqian <89398368@qq.com>
2024-03-08 22:08:40 +0800

    登陆注册完成

--- lanshuqian-music/components/login/login.js
+++ lanshuqian-music/components/login/login.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+
+
+
     $('#loginButton').click(function () {
         window.location.href = "./components/login/login.html";
     });
@@ -6,26 +9,75 @@ $(document).ready(function () {
     $('#loginForm').submit(function (event) {
         event.preventDefault();
 
-        var username = $('#loginUsername').val();
-        var password = $('#loginPassword').val();
+
+        const clickedButtonClass = $(document.activeElement).attr('class');
 
 
-        var clickedButtonClass = $(document.activeElement).attr('class');
+        let a = $("#loginUsername").val();
+        let b = $("#loginPassword").val()
+
 
         if (clickedButtonClass === 'tabs1') {
-            console.log('用户名:', username);
-            console.log('密码:', password);
-            console.log('执行登录操作');
+            $.ajax({
+                type: "POST",
+                url: "http://localhost:8080/api/user/login",
+                data: {
+                    userAccount: a,
+                    userPassword: b,
+                },
+                success: function (response) {
+                    console.log(response);
+                    if (response.code === 0) {
+                        console.log('登录成功');
+                        //保存cookie到本地
+                        let user = JSON.stringify(response.data);
+                        document.cookie = "userLoginState=" + user + "; path= /";
+                        // document.cookie = "userLoginState=" + user;
+                        //登陆成功跳转到主界面
+                        window.location.href = "../../index.html"
+                    } else {
+                        console.log('登录失败:' + response.message)
+                    }
+                },
+                error: function (xhr, status, error) {
+                    console.error(xhr.responseText);
+                }
+            });
 
         } else if (clickedButtonClass === 'tabs2') {
-            console.log('用户名:', username);
-            console.log('密码:', password);
-            console.log('执行注册操作');
-
+            $.ajax({
+                type: "POST",
+                url: "http://localhost:8080/api/user/register",
+                data: {
+                    userAccount: a,
+                    userPassword: b,
+                    checkPassword: b,
+                    planetCode: Math.floor(Math.random() * (9999 - 1) + 1)
+                },
+                success: function (response) {
+                    console.log(response);
+                    if (response.code === 0) {
+                        console.log('注册成功' + response.description);
+                        //注册成功跳转到主界面
+                        window.location.href = "../../index.html"
+                    } else {
+                        console.log('注册失败:' + response.description)
+                    }
+                },
+                error: function (xhr, status, error) {
+                    console.error(xhr.responseText);
+                }
+            });
         }
-
         // 清空输入框
         $('#loginUsername').val('');
         $('#loginPassword').val('');
     });
 });
+
+// 生成介于 min（包括）和 max（不包括）之间的随机整数
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+var randomInt = getRandomInt(1, 99999); // 生成一个介于 1（包括）到 100（不包括）之间的随机整数
