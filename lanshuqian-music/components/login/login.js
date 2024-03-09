3ce17b07ac26a87b615bc49a9cfb5753fac22ad4
lanshuqian <89398368@qq.com>
2024-03-09 11:07:37 +0800

    完成了登陆注册的细节

--- lanshuqian-music/components/login/login.js
+++ lanshuqian-music/components/login/login.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
 
 
-
     $('#loginButton').click(function () {
         window.location.href = "./components/login/login.html";
     });
@@ -33,10 +32,25 @@ $(document).ready(function () {
                         let user = JSON.stringify(response.data);
                         document.cookie = "userLoginState=" + user + "; path= /";
                         // document.cookie = "userLoginState=" + user;
+                        // 登录成功后显示提示框
+                        $('#successMessage').show();
+                        setTimeout(function () {
+                            window.location.href = "../../index.html"
+                        }, 500);
+                        setTimeout(function () {
+                            $('#successMessage').hide();
+                        }, 1000);
+
+
                         //登陆成功跳转到主界面
-                        window.location.href = "../../index.html"
                     } else {
                         console.log('登录失败:' + response.message)
+                        //显示登录失败提示框
+                        $('#errorMessage').text('登录失败: ' + response.message).show();
+                        // 延迟1秒后隐藏错误提示框
+                        setTimeout(function () {
+                            $('#errorMessage').hide();
+                        }, 1000); // 1000毫秒 = 1秒
                     }
                 },
                 error: function (xhr, status, error) {
@@ -58,10 +72,42 @@ $(document).ready(function () {
                     console.log(response);
                     if (response.code === 0) {
                         console.log('注册成功' + response.description);
-                        //注册成功跳转到主界面
-                        window.location.href = "../../index.html"
+                        $('#registerSuccessMessage').show();
+                        setTimeout(function () {
+                            $('#successMessage').hide();
+                        }, 1000);
+                        //注册成功直接缓存cookie
+                        $.ajax({
+                            type: "POST",
+                            url: "http://localhost:8080/api/user/login",
+                            data: {
+                                userAccount: a,
+                                userPassword: b,
+                            },
+                            success: function (response) {
+                                console.log(response);
+                                if (response.code === 0) {
+                                    //保存cookie到本地
+                                    let user = JSON.stringify(response.data);
+                                    document.cookie = "userLoginState=" + user + "; path= /";
+                                }
+                            },
+                            error: function (xhr, status, error) {
+                                console.error(xhr.responseText);
+                            }
+                        });
+                        //
+                        setTimeout(function () {
+                            window.location.href = "../../index.html"
+                        }, 500);
                     } else {
-                        console.log('注册失败:' + response.description)
+                        console.log('注册失败: ' + response.description);
+                        //显示注册失败提示框
+                        $('#registerFailMessage').text('注册失败: ' + response.description).show();
+                        // 延迟1秒后隐藏错误提示框
+                        setTimeout(function () {
+                            $('#registerFailMessage').hide();
+                        }, 1000); // 1000毫秒 = 1秒
                     }
                 },
                 error: function (xhr, status, error) {
@@ -69,9 +115,13 @@ $(document).ready(function () {
                 }
             });
         }
+
+
         // 清空输入框
         $('#loginUsername').val('');
         $('#loginPassword').val('');
+
+
     });
 });
 
